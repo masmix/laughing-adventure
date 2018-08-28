8869e04b9d82f0b72f52e88cafc2ed7cac171cba
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-08-28 13:28:32 +0200

    fix to another script docker

--- docker-compose-advanced-tasks/hello-flask/run-container.py
+++ docker-compose-advanced-tasks/hello-flask/run-container.py
@@ -1,4 +1,10 @@
+#import docker
+#client = docker.from_env()
+#container = client.containers.run("bfirsh/reticulate-splines", detach=True)
+#print container.id
 import docker
 client = docker.from_env()
-container = client.containers.run("bfirsh/reticulate-splines", detach=True)
-print container.id
+container = client.containers.get('f1064a8a4c82')
+print container.logs()
+
+
