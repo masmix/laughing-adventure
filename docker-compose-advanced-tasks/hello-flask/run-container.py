82ce3174e54dc2cbfda62b6ac287f63c7d86227f
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-08-28 15:38:56 +0200

    adding changes to install docker python library to image and changing script

--- docker-compose-advanced-tasks/hello-flask/run-container.py
+++ docker-compose-advanced-tasks/hello-flask/run-container.py
@@ -4,7 +4,8 @@
 #print container.id
 import docker
 client = docker.from_env()
-container = client.containers.get('f1064a8a4c82')
-print container.logs()
+print dir(client)
+#container = client.containers.get('f1064a8a4c82')
+#print container.logs()
 
 
