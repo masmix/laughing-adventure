5d8f5781d2159936feb3495fb58b95af7ec5717e
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-08-28 16:28:59 +0200

    tring to get list of object atributes

--- docker-compose-advanced-tasks/hello-flask/run-container.py
+++ docker-compose-advanced-tasks/hello-flask/run-container.py
@@ -4,9 +4,9 @@
 #print container.id
 import docker
 client = docker.from_env()
-print dir(client)
-for image in client.images.list():
-  print image.id
+print dir(client.images)
+#for image in dir(client.images):
+#  print image.id
 #container = client.containers.get('f1064a8a4c82')
 #print container.logs()
 
