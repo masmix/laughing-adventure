a80da8f063470f45a696651215d8acd1a3dd15c4
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-09-03 08:44:05 +0200

    fix to run command line using 192.168.56.101 host

--- docker-compose-advanced-tasks/hello-flask/run-container.py
+++ docker-compose-advanced-tasks/hello-flask/run-container.py
@@ -1,5 +1,7 @@
-from docker import Client
-cli = Client(base_url='http://localhost:2375')
+import docker 
+#li = Client(base_url='http://localhost:2375')
+cli = docker.from_env(base_url='http://192.168.56.101:2375')
+
 print cli.containers()
 #client.inspect_container('romantic_feynman')['State']['Status']
 is_romantic_feynman = cli.inspect_container('romantic_feynman')['State']['Status']
