f8457b158fa7faac9c3eb54c1b47355d4fa54b31
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-09-03 09:02:30 +0200

    tring get specific container status

--- docker-compose-advanced-tasks/hello-flask/run-container.py
+++ docker-compose-advanced-tasks/hello-flask/run-container.py
@@ -1,12 +1,12 @@
 import docker 
-print dir(docker.client)
+#print dir(docker.client)
 client = docker.APIClient(base_url='tcp://192.168.56.101:2375')
-print client.version()
+#print client.version()
 
 #li = Client(base_url='http://localhost:2375')
 #cli = docker.from_env(base_url='http://192.168.56.101:2375')
 
 #print cli.containers()
 #client.inspect_container('romantic_feynman')['State']['Status']
-#is_romantic_feynman = cli.inspect_container('romantic_feynman')['State']['Status']
-#print is_romantic_feynman
+is_romantic_feynman = cli.inspect_container('romantic_feynman')['State']['Status']
+print is_romantic_feynman
