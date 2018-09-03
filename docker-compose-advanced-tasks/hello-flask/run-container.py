3ba3d209a4f71196532a7bf32000a2358fdb8e35
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-09-03 08:25:43 +0200

    adding code to get container statsis

--- docker-compose-advanced-tasks/hello-flask/run-container.py
+++ docker-compose-advanced-tasks/hello-flask/run-container.py
@@ -1,3 +1,6 @@
 from docker import Client
 cli = Client(base_url='http://localhost:2375')
 print cli.containers()
+#client.inspect_container('romantic_feynman')['State']['Status']
+is_romantic_feynman = cli.inspect_container('romantic_feynman')['State']['Status']
+print is_romantic_feynman
