2a9e139b36be9a2859271066ace1bddf4a6a0d0a
wojtek <wojciech.kaczmarczyk@capgemini.com>
2018-09-07 14:44:14 +0200

    changes to make docker registry on with IP for Host

--- docker-swarm/hello-docker-swarm/run-registry.sh
+++ docker-swarm/hello-docker-swarm/run-registry.sh
@@ -6,8 +6,8 @@ docker run -d \
   --restart=always \
   --name registry \
   -v $CERT_ROOT_PATH:/certs \
-  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/$CERT_TLS_CERTIFICATE \
   -e REGISTRY_HTTP_TLS_KEY=/certs/$CERT_TLS_KEY  \
-  -p 443:443 \
+  -p 5000:5000 \
   registry:2
--- docker-swarm/hello-docker-swarm/tools/make-tls-certs-for-docker-local-registry.sh
+++ docker-swarm/hello-docker-swarm/tools/make-tls-certs-for-docker-local-registry.sh
@@ -1,1 +1,1 @@
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout centos4.localdomain.key -x509 -days 365 -out centos4.localdomain.crt
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout centos4.localdomain.key -x509 -days 365 -out centos4.localdomain.crt -config san.cnf
